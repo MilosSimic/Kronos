f1a4050610b38241e967bc3b1208ba1a400a5f7d
milossimic <milossimicsimo@gmail.com>
2015-12-20 19:38:32 +0100

    full google like cron job grammar

--- model.py
+++ model.py
@@ -13,7 +13,13 @@ kron = Kronos()
 for job in kron.model.jobs:
 	print job.desc.content
 	print job.url.location
-	print "every {} {}".format(job.schedule.n, job.schedule.unit)
-	if job.schedule.when:
-		print "from {} to {}".format(job.schedule.when.start, job.schedule.when.end)
+
+	if hasattr(job.schedule, 'ordinal'):
+		print job.schedule.ordinal
+		print job.schedule.days
+		print job.schedule.monthspec.months
+		print job.schedule.time
+	else:
+		print 'nema'
+
 	print
